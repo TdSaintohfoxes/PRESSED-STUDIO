4068b34d11a22582420bd08912d518026fede37f
TdSaintohfoxes <tayob9097@gmail.com>
2026-08-28 19:17:34 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pressed-v21';
+const CACHE='pressed-v22';
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']).catch(()=>{}));
   self.skipWaiting();
